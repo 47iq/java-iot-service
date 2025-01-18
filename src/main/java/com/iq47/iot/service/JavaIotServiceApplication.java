2ee18f4f2e8f83b9a9bb5883a3593ac8366bf1d8
Pavel Danilov <pasha.danilov.perm@gmail.com>
2025-01-18 17:53:56 +0300

    feature: init project

---
+++ src/main/java/com/iq47/iot/service/JavaIotServiceApplication.java
@@ -0,0 +1,13 @@
+package com.iq47.iot.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaIotServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaIotServiceApplication.class, args);
+    }
+
+}
